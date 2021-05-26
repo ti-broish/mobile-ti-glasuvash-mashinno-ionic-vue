1d87ea7b5f9c36832e92d18441b45cf6deceadb9
Krasimir Slavkov <muxozavar@yahoo.com>
2021-05-26 22:52:16 +0300

    Create Ionic vue capacitor project

---
+++ ti-glasuvash-mashinno/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ti-glasuvash-mashinno',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
