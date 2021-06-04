9e5473ac97abbb0aff50e4adc0b3fcb90ae7dad2
Krasimir Slavkov <muxozavar@yahoo.com>
2021-06-04 13:39:04 +0300

    Implement android back button

--- ti-glasuvash-mashinno/src/App.vue
+++ ti-glasuvash-mashinno/src/App.vue
@@ -5,14 +5,29 @@
 </template>
 
 <script lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { IonApp, IonRouterOutlet, useBackButton } from "@ionic/vue";
+import { defineComponent } from "vue";
+import { useRouter } from "vue-router";
+import { App } from "@capacitor/app";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
     IonApp,
-    IonRouterOutlet
-  }
+    IonRouterOutlet,
+  },
+  setup() {
+    const router = useRouter();
+
+    useBackButton(10, () => {
+      if (router.currentRoute.value.path === "/parties") {
+        router.replace("/home");
+      } else if (router.currentRoute.value.path === "/preview") {
+        router.replace("/parties");
+      } else {
+        App.exitApp();
+      }
+    });
+  },
 });
 </script>
